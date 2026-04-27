011577c2bdb2011f76489e665c3e38ef3a938375
Mahmoud <mahmoudalshehyby@gmail.com>
2026-04-27 05:47:33 +0300

    Build Assertion messages package

---
+++ lib/utils/assertion_msg.dart
@@ -0,0 +1,6 @@
+// ignore_for_file: constant_identifier_names
+
+const String SHAPE_ASSERT =
+    "decoration.shape != BoxShape.circle || decoration.borderRadius == null is not true";
+const String SCALE_ASSERT = "scale >= 0.0 is not true";
+const String SIZE_ASSERT = "width >= 0.0 && height >= 0.0 is not true";
